0bc03502c688433778712d5f16333bcd10787651
AnshikaB-25 <anshikabansal2510@gmail.com>
2025-06-25 21:58:09 +0530

    Update subarray_sum_equals_k.cpp

--- 03_array/subarray_sum_equals_k.cpp
+++ 03_array/subarray_sum_equals_k.cpp
@@ -16,3 +16,37 @@ public:
         return count;
     }
 };
+// better:
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        for(int i = 0; i<n ; i++){
+            int sum = 0;
+            for(int j = i ; j<n; j++){
+                    sum += nums[x];
+                    if(sum==k) count++;
+            }
+        }
+        return count;
+    }
+};
+// OPTIMAL : DOUBT
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+        int sum = 0;
+       unordered_map<int , int> mpp;
+        mpp[0]=1;
+        for (int i = 0; i < n; i++) {
+            sum+=nums[i];
+            int remove = sum - k;
+            count+=mpp[remove];
+            mpp[sum]+=1;    
+        }
+        return count;
+    }
+};
