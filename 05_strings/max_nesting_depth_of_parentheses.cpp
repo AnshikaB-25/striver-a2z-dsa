9fdf6dfd0123f9a14ef1ad791b5203b6523bd595
AnshikaB-25 <anshikabansal2510@gmail.com>
2025-07-19 20:56:56 +0530

    Create max_nesting_depth_of_parentheses.cpp

---
+++ 05_strings/max_nesting_depth_of_parentheses.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n = s.size();
+        int count = 0;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == '(') {
+                count++;
+                ans = max(count, ans);
+            } else if (s[i] == ')')
+                count--;
+        }
+        return ans;
+    }
+};
