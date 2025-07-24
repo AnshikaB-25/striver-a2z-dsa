d7200cb08361d0d32b2aa1db5cadd9e0134f8020
AnshikaB-25 <anshikabansal2510@gmail.com>
2025-07-24 19:45:13 +0530

    Update palindrome_linked_list.cpp

--- 06_linked_list/palindrome_linked_list.cpp
+++ 06_linked_list/palindrome_linked_list.cpp
@@ -18,3 +18,37 @@ public:
         
     }
 };
+// OPTIMAL APPROACH - REVERSE THE SECOND HALF AND CHECK
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        while (curr != NULL) {
+            ListNode* nextnode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextnode;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* newNode = reverseList(slow);
+        while (head != NULL && newNode != NULL) {
+            if (head->val == newNode->val) {
+                head = head->next;
+                newNode = newNode->next;
+            } else
+                return false;
+        }
+        return true;
+    }
+};
